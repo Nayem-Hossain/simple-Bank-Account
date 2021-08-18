15a056a3e531c0553b7a924129eee7ce41ca492e
Nayem-Hossain <nayem.hossain7298@gmail.com>
2021-08-19 00:01:20 +0600

    first commit

---
+++ AccountCalculation.js
@@ -0,0 +1,72 @@
+//validation function
+function inputValidation() {
+  const inputValue = document.getElementById("input-money").value;
+  if (inputValue == "") {
+    return "please, enter the ammount that you want to deposite or withdraw";
+  } else if (isNaN(inputValue)) {
+    return "string is not valid ammount, please enter a vilid ammount";
+  } else if (parseInt(inputValue) < 0) {
+    return "negative number is not valid ammount, please enter a vilid ammount";
+  }
+}
+//validation function
+function ammountValidation() {
+  const inputWithdrawBalance = document.getElementById("input-money").value;
+  const totalBalence = document.getElementById("balance-total").innerText;
+  if (inputWithdrawBalance > totalBalence) {
+    return (
+      "sorry! you have not sufficient balance for withdraw " +
+      inputWithdrawBalance +
+      " taka"
+    );
+  }
+}
+//get input Balance
+function getInputAmmount(inputBalanceId) {
+  const inputBalance = parseFloat(
+    document.getElementById(inputBalanceId).value
+  );
+  document.getElementById(inputBalanceId).value = "";
+  return inputBalance;
+}
+//get current Balance
+function getCurrentAmmount(currentBalanceId, inputBalance) {
+  const currentAmmount = parseFloat(
+    document.getElementById(currentBalanceId).innerText
+  );
+  document.getElementById(currentBalanceId).innerText = parseFloat(
+    currentAmmount + inputBalance
+  );
+}
+// deposite button event handler
+document
+  .getElementById("deposit-button")
+  .addEventListener("click", function () {
+    if (inputValidation()) {
+      alert(inputValidation());
+      document.getElementById("input-money").value = "";
+      return;
+    } else {
+      const inputBalance = getInputAmmount("input-money");
+      getCurrentAmmount("deposite-total", inputBalance);
+      getCurrentAmmount("balance-total", inputBalance);
+    }
+  });
+// withdraw button event handler
+const withdrawBtn = document.getElementById("withdraw-button");
+withdrawBtn.addEventListener("click", function () {
+  if (inputValidation()) {
+    alert(inputValidation());
+    document.getElementById("input-money").value = "";
+    return;
+  }
+  if (ammountValidation()) {
+    alert(ammountValidation());
+    document.getElementById("input-money").value = "";
+    return;
+  } else {
+    const inputBalance = getInputAmmount("input-money");
+    getCurrentAmmount("withdraw-total", inputBalance);
+    getCurrentAmmount("balance-total", -inputBalance);
+  }
+});
